a27ec898b2556d214702cb7e73d186ce491f7d88
Allen Lin <allenlinsh@gmail.com>
2024-11-09 14:23:06 -0500

    check .env variable in example

--- examples/send_text.py
+++ examples/send_text.py
@@ -7,6 +7,10 @@ load_dotenv()
 
 # Set your API key
 textfully.api_key = os.getenv("TEXTFULLY_API_KEY")
+
+if not textfully.api_key:
+    raise ValueError("TEXTFULLY_API_KEY environment variable is not set")
+
 to_number = (
     "your-verified-phone-number"  # Include international code (e.g. +1 for US/Canada)
 )
